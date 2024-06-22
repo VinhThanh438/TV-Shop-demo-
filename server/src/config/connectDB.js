c679fb74a83e35f8ccc116ddaee6cc11943ae9df
Thanh Vinh <vuthanhvinh438@gmail.gmailcom>
2024-06-22 17:24:47 +0700

    config deploy

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    user: 'root',
-    database: 'tv_shop',
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
     waitForConnections: true,
 });
 
